8aea884d3aa1a71ae2646bffad4fea317455e8e4
vishal chaudhary <vishal@angularminds.in>
2022-01-05 18:16:47 +0530

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { JokesComponent } from './jokes/jokes.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    JokesComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
